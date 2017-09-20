71045cc90347eebc1ad848da4b6b55aecd89c538
EridiahMods <eridiahmods@gmail.com>
2017-09-20 16:46:28 -0400

    Fixed an error in BlockConnectedTextures

--- src/main/java/com/eridianrpg/common/blocks/BlockConnectedTextures.java
+++ src/main/java/com/eridianrpg/common/blocks/BlockConnectedTextures.java
@@ -15,7 +15,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
 public class BlockConnectedTextures extends Block {
-	
+
     public static final PropertyBool CONNECTED_DOWN = PropertyBool.create("connected_down");
     public static final PropertyBool CONNECTED_UP = PropertyBool.create("connected_up");
     public static final PropertyBool CONNECTED_NORTH = PropertyBool.create("connected_north");
@@ -30,27 +30,27 @@ public class BlockConnectedTextures extends Block {
 		this.setDefaultState(this.blockState.getBaseState().withProperty(CONNECTED_DOWN, Boolean.FALSE).withProperty(CONNECTED_EAST, Boolean.FALSE).withProperty(CONNECTED_NORTH, Boolean.FALSE).withProperty(CONNECTED_SOUTH, Boolean.FALSE).withProperty(CONNECTED_UP, Boolean.FALSE).withProperty(CONNECTED_WEST, Boolean.FALSE));
 		this.setSoundType(type);
 	}
-	
+
 	@Override
 	public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos position) {
-		return state.withProperty(CONNECTED_DOWN, this.isSideConnectable(world, position, EnumFacing.DOWN)).withProperty(CONNECTED_EAST, this.isSideConnectable(world, position, EnumFacing.EAST)).withProperty(CONNECTED_NORTH, this.isSideConnectable(world, position, EnumFacing.NORTH)).withProperty(CONNECTED_SOUTH, this.isSideConnectable(world, position, EnumFacing.SOUTH)).withProperty(CONNECTED_UP, this.isSideConnectable(world, position, EnumFacing.UP)).withProperty(CONNECTED_WEST, this.isSideConnectable(world, position, EnumFacing.WEST));
+		return state.withProperty(CONNECTED_DOWN, this.isSideConnectable(world, position, EnumFacing.DOWN)).withProperty(CONNECTED_EAST, this.isSideConnectable(world, position, EnumFacing.EAST)).withProperty(CONNECTED_NORTH, this.isSideConnectable(world, position, EnumFacing.NORTH)).withProperty(CONNECTED_SOUTH, this.isSideConnectable(world, position, EnumFacing.SOUTH)).withProperty(CONNECTED_UP, this.isSideConnectable(world, position, EnumFacing.UP)).withProperty(CONNECTED_WEST,  this.isSideConnectable(world, position, EnumFacing.WEST));
 	}
-	
+
 	@Override
 	protected BlockStateContainer createBlockState() {
 		return new BlockStateContainer(this, new IProperty[] { CONNECTED_DOWN, CONNECTED_UP, CONNECTED_NORTH, CONNECTED_SOUTH, CONNECTED_WEST, CONNECTED_EAST });
 	}
-	
+
     @Override
     public int getMetaFromState (IBlockState state) {
-        
+
         return 0;
     }
-    
+
     private boolean isSideConnectable (IBlockAccess world, BlockPos pos, EnumFacing side) {
-        
+
         final IBlockState state = world.getBlockState(pos.offset(side));
         return (state == null) ? false : state.getBlock() == this;
     }
-	
+
 }
